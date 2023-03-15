4b051fb2212e62cdffe3d6d2a9ef275ccb5dcfd1
Sachin Ronson Dsilva <110468373+sachindsilva16@users.noreply.github.com>
2023-03-15 23:15:18 +0530

    Update App.jsx

--- src/ReactComponents/App.jsx
+++ src/ReactComponents/App.jsx
@@ -15,7 +15,7 @@ function App() {
                     {/* MOODLE */}
                     <div className="flex items-center flex-col border-1 mb-12 mt-4 h-[300px] p-8 justify-center rounded-md shadow-md shadow-[#C0C0C0]">
                         <p className="mb-4 ml-4 mr-4 text-xl font-semibold text-[#C0C0C0]">NMAMIT Moodle</p>
-                        <a href="http://guru.nmamit.in" className="">
+                        <a href="http://guru.nmamit.in" >
                             <button className="transition duration-50 ease-in-out active:translate-y-1 active:scale-110 bg-blue-200 w-[180px] h-10 rounded-md text-black bg-gradient-to-r from-blue-600 to-green-400 shadow-lg shadow-green-400/50 font-medium active:text-[#C0C0C0] active:cursor-pointer  hover:cursor-pointer
                             relative"><span className="relative top-[7px]">Click here</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="20px" height="20px" className="invert-gray-200 relative left-[150px]
@@ -24,8 +24,8 @@ function App() {
                         <div className="mt-8">
                             <p className="mb-2 ml-4 mr-4 font-medium text-[#C0C0C0]">[Alternates]</p>
                         </div>
-                        <a href="http://115.243.167.87" target="_blank">
-                            <button className=" bg-blue-200 w-[180px] h-10 mt-2 rounded-md text-black bg-gradient-to-r from-blue-600 to-green-400 shadow-lg shadow-green-400/50 font-medium active:bg-black active:text-[#C0C0C0] active:cursor-pointer hover:cursor-pointer
+                        <a href="http://115.243.167.87" >
+                            <button className="transition duration-50 ease-in-out active:translate-y-1 active:scale-110 bg-blue-200 w-[180px] h-10 mt-2 rounded-md text-black bg-gradient-to-r from-blue-600 to-green-400 shadow-lg shadow-green-400/50 font-medium active:bg-black active:text-[#C0C0C0] active:cursor-pointer hover:cursor-pointer
                             relative"><span className="relative top-[7px]">Click here</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="20px" height="20px" className="invert-gray-200 relative left-[150px]
                             bottom-[19px]"><path d="M 40 10 C 38.896 10 38 10.896 38 12 C 38 13.104 38.896 14 40 14 L 47.171875 14 L 30.585938 30.585938 C 29.804938 31.366938 29.804938 32.633063 30.585938 33.414062 C 30.976938 33.805063 31.488 34 32 34 C 32.512 34 33.023063 33.805062 33.414062 33.414062 L 50 16.828125 L 50 24 C 50 25.104 50.896 26 52 26 C 53.104 26 54 25.104 54 24 L 54 12 C 54 10.896 53.104 10 52 10 L 40 10 z M 18 12 C 14.691 12 12 14.691 12 18 L 12 46 C 12 49.309 14.691 52 18 52 L 46 52 C 49.309 52 52 49.309 52 46 L 52 34 C 52 32.896 51.104 32 50 32 C 48.896 32 48 32.896 48 34 L 48 46 C 48 47.103 47.103 48 46 48 L 18 48 C 16.897 48 16 47.103 16 46 L 16 18 C 16 16.897 16.897 16 18 16 L 30 16 C 31.104 16 32 15.104 32 14 C 32 12.896 31.104 12 30 12 L 18 12 z" /></svg></button>
@@ -36,8 +36,8 @@ function App() {
                     {/* SMART CAMPUS  */}
                     <div className="flex items-center flex-col mb-8 border-1 h-[190px] px-10 justify-center rounded-md shadow-md shadow-[#C0C0C0]">
                         <p className="mb-4 ml-4 mr-4 text-xl font-semibold text-[#C0C0C0]">Smart Campus</p>
-                        <a href="http://parent-login.nmamit.in/DashBoard" target="_blank">
-                            <button className=" bg-blue-200 w-[180px] h-10 rounded-md text-black bg-gradient-to-r from-blue-600 to-green-400 shadow-lg shadow-green-400/50 font-medium active:bg-black active:text-[#C0C0C0] active:cursor-pointer hover:cursor-pointer
+                        <a href="http://parent-login.nmamit.in/DashBoard" >
+                            <button className="transition duration-50 ease-in-out active:translate-y-1 active:scale-110 bg-blue-200 w-[180px] h-10 rounded-md text-black bg-gradient-to-r from-blue-600 to-green-400 shadow-lg shadow-green-400/50 font-medium active:bg-black active:text-[#C0C0C0] active:cursor-pointer hover:cursor-pointer
                             relative"><span className="relative top-[7px]">Click here</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="20px" height="20px" className="invert-gray-200 relative left-[150px]
                             bottom-[19px]"><path d="M 40 10 C 38.896 10 38 10.896 38 12 C 38 13.104 38.896 14 40 14 L 47.171875 14 L 30.585938 30.585938 C 29.804938 31.366938 29.804938 32.633063 30.585938 33.414062 C 30.976938 33.805063 31.488 34 32 34 C 32.512 34 33.023063 33.805062 33.414062 33.414062 L 50 16.828125 L 50 24 C 50 25.104 50.896 26 52 26 C 53.104 26 54 25.104 54 24 L 54 12 C 54 10.896 53.104 10 52 10 L 40 10 z M 18 12 C 14.691 12 12 14.691 12 18 L 12 46 C 12 49.309 14.691 52 18 52 L 46 52 C 49.309 52 52 49.309 52 46 L 52 34 C 52 32.896 51.104 32 50 32 C 48.896 32 48 32.896 48 34 L 48 46 C 48 47.103 47.103 48 46 48 L 18 48 C 16.897 48 16 47.103 16 46 L 16 18 C 16 16.897 16.897 16 18 16 L 30 16 C 31.104 16 32 15.104 32 14 C 32 12.896 31.104 12 30 12 L 18 12 z" /></svg></button>
@@ -51,8 +51,8 @@ function App() {
                     <div className="flex items-center flex-col mb-8 border-1 h-[190px] px-0 justify-center rounded-md shadow-md shadow-[#C0C0C0]">
                         <p className="mb-4 ml-0 mr-0 text-xl font-semibold text-[#C0C0C0] text-center px-8">ISE-Study Resources
                         <span className="font-normal text-sm block">Telegram</span></p>
-                        <a href="https://t.me/ise_nmamit" target="_blank">
-                            <button className=" bg-blue-200 w-[180px] h-10 rounded-md text-black bg-gradient-to-r from-blue-600 to-green-400 shadow-lg shadow-green-400/50 font-medium active:bg-black active:text-[#C0C0C0] active:cursor-pointer hover:cursor-pointer
+                        <a href="https://t.me/ise_nmamit" >
+                            <button className="transition duration-50 ease-in-out active:translate-y-1 active:scale-110 bg-blue-200 w-[180px] h-10 rounded-md text-black bg-gradient-to-r from-blue-600 to-green-400 shadow-lg shadow-green-400/50 font-medium active:bg-black active:text-[#C0C0C0] active:cursor-pointer hover:cursor-pointer
                             relative"><span className="relative top-[7px]">Click here</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="20px" height="20px" className="invert-gray-200 relative left-[150px]
                             bottom-[19px]"><path d="M 40 10 C 38.896 10 38 10.896 38 12 C 38 13.104 38.896 14 40 14 L 47.171875 14 L 30.585938 30.585938 C 29.804938 31.366938 29.804938 32.633063 30.585938 33.414062 C 30.976938 33.805063 31.488 34 32 34 C 32.512 34 33.023063 33.805062 33.414062 33.414062 L 50 16.828125 L 50 24 C 50 25.104 50.896 26 52 26 C 53.104 26 54 25.104 54 24 L 54 12 C 54 10.896 53.104 10 52 10 L 40 10 z M 18 12 C 14.691 12 12 14.691 12 18 L 12 46 C 12 49.309 14.691 52 18 52 L 46 52 C 49.309 52 52 49.309 52 46 L 52 34 C 52 32.896 51.104 32 50 32 C 48.896 32 48 32.896 48 34 L 48 46 C 48 47.103 47.103 48 46 48 L 18 48 C 16.897 48 16 47.103 16 46 L 16 18 C 16 16.897 16.897 16 18 16 L 30 16 C 31.104 16 32 15.104 32 14 C 32 12.896 31.104 12 30 12 L 18 12 z" /></svg></button>
@@ -62,8 +62,6 @@ function App() {
                     </div>
 
                 </div>
-
-        <button className="hover:transition hover:duration-150 hover:ease-in-out hover:translate-y-1 hover:scale-110 bg-blue-500 rounded-md p-2 w-24 h-10">Click Me</button>
 
             </div>
         </div>
