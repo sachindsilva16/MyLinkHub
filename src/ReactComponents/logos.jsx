ae41b91b277bff653ef145de6470e513a21d253b
Sachin Ronson Dsilva <110468373+sachindsilva16@users.noreply.github.com>
2023-03-16 20:55:46 +0530

    Update logos.jsx

--- src/ReactComponents/logos.jsx
+++ src/ReactComponents/logos.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Logo(props) {
     return (
-        <a href={props.href} className="animate-spin hover:animate-none cursor-pointer pb-2 px-2">
-            <img className=" hover:grayscale hover:scale-110 h-[40px] w-[40px] rounded-full " src={props.src} />
+        <a href={props.href} className="animate-spin hover:animate-none hover:scale-110 cursor-pointer pb-2 px-2">
+            <img className=" hover:grayscale hover:scale-110 active:scale-110 h-[40px] w-[40px] rounded-full " src={props.src} />
         </a>
     );
 }
