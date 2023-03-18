12c8357285f1d294ad18fed211d5dc9303563ef1
Sachin Ronson Dsilva <110468373+sachindsilva16@users.noreply.github.com>
2023-03-18 21:17:39 +0530

    Add files via upload

--- src/ReactComponents/Footer.jsx
+++ src/ReactComponents/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Credits from "./Credits"
-import Logo from "./SocialMedia";
+import Logo from "./Logo";
+import TelegramLogo from "./TelegramLogo";
 
 function Footer() {
     return (
@@ -9,15 +10,22 @@ function Footer() {
                 items-center">
 
                 {/* <a href="https://github.com/sachindsilva16" className="animate-spin hover:animate-none cursor-pointer pb-2 px-2 "> */}
-                <Logo href="https://github.com/sachindsilva16" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" />
+
                 {/* </a> */}
+                <Logo href="https://www.instagram.com/sachin.dsilva_/" src="https://raw.githubusercontent.com/sachindsilva16/Hyperlinks-ReactApp/1720dc2d04802469450e620f9502475d5d701b46/img/instagram.svg" />
 
 
-                <Logo href="https://www.linkedin.com/in/sachin-dsilva-43169a1bb" src="https://www.freepnglogos.com/uploads/linkedin-logo-transparent----6.png" />
 
-                <Logo href="https://www.instagram.com/sachin.dsilva_/" src="https://www.freepnglogos.com/uploads/512x512-logo/512x512-transparent-circle-instagram-media-network-social-logo-new-16.png" />
 
-                <Logo href="https://twitter.com/sachinrons123" src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-icon-circle-png-logo-8.png" />
+                <Logo href="https://github.com/sachindsilva16" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" />
+
+
+                <Logo href="https://www.linkedin.com/in/sachin-dsilva-43169a1bb" src="https://raw.githubusercontent.com/sachindsilva16/Hyperlinks-ReactApp/468867e6ae52bad1a6b021193416c2857ecb311f/img/linkedin.svg" />
+
+
+                <Logo href="https://twitter.com/sachinrons123" src="https://raw.githubusercontent.com/sachindsilva16/Hyperlinks-ReactApp/54bdf5e79435f9df86a4704966a6c47c43811c0f/img/twitter.svg"  />
+
+                <TelegramLogo href="t.me/ise_nmamit/" src="https://raw.githubusercontent.com/sachindsilva16/Hyperlinks-ReactApp/49e4f0b1e2c193942538e1233f5c20095d8b0da2/img/telegram.svg"/>
             </div>
             <Credits />
         </footer>
