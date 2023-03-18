45354fd5d7e89c9019960a9e48f769f557891343
Sachin Ronson Dsilva <110468373+sachindsilva16@users.noreply.github.com>
2023-03-18 17:00:50 +0530

    Update WebLab.jsx

--- src/ReactComponents/WebLab.jsx
+++ src/ReactComponents/WebLab.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 function WebLab() {
     return (
 
-        <div className="flex items-center flex-col mb-8 border-1 h-[190px] px-0 justify-center rounded-md shadow-md shadow-[#C0C0C0] md:mx-6 md:h-[300px]">
+        <div className="flex items-center flex-col mb-8 border-1 h-[190px] px-8 md:px-8 justify-center rounded-md shadow-md shadow-[#C0C0C0] md:mx-6 md:h-[300px]">
             <p className="mb-4 ml-0 mr-0 text-xl font-semibold text-[#C0C0C0] text-center px-[30px]  ">Web Lab MSE1
                 <span className="font-normal text-sm block">Source Code</span></p>
             <a href="https://github.com/sachindsilva16/WebP-Lab-Code/tree/main/WEB_MSE01#readme" >
