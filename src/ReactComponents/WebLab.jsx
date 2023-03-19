5ff9bd0c65469b4a81b46fe87c89afa7c8b823c1
Sachin Ronson Dsilva <110468373+sachindsilva16@users.noreply.github.com>
2023-03-20 00:08:59 +0530

    Update WebLab.jsx

--- src/ReactComponents/WebLab.jsx
+++ src/ReactComponents/WebLab.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import DownloadButton from "./DownloadButton";
 
 function WebLab(){
     return (
@@ -12,11 +13,12 @@ function WebLab(){
 
         // </div>
 
-<div className="flex items-center flex-col mb-8 border-1 h-[190px] px-8 md:px-8 justify-center rounded-md shadow-md shadow-[#C0C0C0] md:mx-6 md:h-[300px]">
-<p className="mb-4 ml-0 mr-0 text-xl font-semibold text-[#C0C0C0] text-center px-[30px]  ">Web Lab MSE1
+<div className="flex items-center flex-col mb-8 border-1 h-[240px] px-8 md:px-8 justify-center rounded-md shadow-md shadow-[#C0C0C0] md:mx-6 md:h-[300px]">
+<p className="mb-4 ml-0 mt-12 mr-0 text-xl font-semibold text-[#C0C0C0] text-center px-[30px]  ">Web Lab MSE1
     <span className="font-normal text-sm block">Source Code</span></p>
-<a href="https://github.com/sachindsilva16/WebP-Lab-Codes/blob/main/WEB_MSE01/readme.md" >
+<a href="https://sachindsilva16.github.io/WebP-Lab-Codes/WEB_MSE01.rar" >
     <Button/>
+    <DownloadButton/>
 </a>
 
 
