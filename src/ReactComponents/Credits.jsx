b18b78bb5412fccbcc55e98e9f73490ca5ee7f60
sachindsilva16 <sachindsilvad@gmail.com>
2023-04-28 23:33:04 +0530

    Modified Footer

--- src/ReactComponents/Credits.jsx
+++ src/ReactComponents/Credits.jsx
@@ -3,10 +3,7 @@ import React from "react";
 function Credits(){
     return(
         <div className="block text-center px-2 py-8 mb-4 md:px-2 md:py-8 md:mb-4">
-<<<<<<< HEAD
             
-=======
->>>>>>> 8ea9bbab141db7665fd82d3f2de1df26b724d0ac
             <p>Made with <span className=" ">❤</span> by <a className="hover:underline hover:text-violet-700 hover:duration-200 transition hover:scale-105" href="https://github.com/sachindsilva16" target="_blank">Sachin Dsilva</a></p>
         </div>
     );
