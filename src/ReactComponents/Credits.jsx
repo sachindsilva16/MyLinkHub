73f1e674840954b7d8db82cc9970ec45d3e30583
sachindsilva16 <sachindsilvad@gmail.com>
2023-04-28 23:17:17 +0530

    Initial Commit:

--- src/ReactComponents/Credits.jsx
+++ src/ReactComponents/Credits.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Credits(){
     return(
-        <div className="block text-center px-2 py-8 mb-4">
+        <div className="block text-center px-2 py-8 mb-4 md:px-2 md:py-8 md:mb-4">
             <p>Made with <span className=" ">❤</span> by <a className="hover:underline hover:text-violet-700 hover:duration-200 transition hover:scale-105" href="https://github.com/sachindsilva16" target="_blank">Sachin Dsilva</a></p>
         </div>
     );
