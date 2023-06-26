1690a0b9fd7e9b5f038190218b5527260e75bab2
Sachin <letscode16@gmail.com>
2023-06-26 23:11:08 +0530

    Added Local Environment

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/*.{html}','./src/ReactComponents/*.jsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
