2239baf55b0bd11d40c43189a799c97f23b0f052
サチン (Sachin) <110468373+sachindsilva16@users.noreply.github.com>
2024-03-29 17:23:46 +0000

    Modified padding,margin and links

--- src/ReactComponents/Credits.jsx
+++ src/ReactComponents/Credits.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Credits(){
     return(
-        <div className=" text-center px-10 py-10 mb-4 md:px-2 md:py-8 md:m-4 lg:mb-8 lg:pb-6">
+        <div className=" text-center px-10 py-12 mb-4 md:px-2 md:py-8 md:m-4 lg:mb-8 lg:py-10">
             
             <p>Made with <span className=" ">❤</span> by <a className="hover:underline hover:text-violet-700 hover:duration-200 transition hover:scale-105" href="https://github.com/sachindsilva16"  >Sachin Dsilva</a></p>
         </div>
--- src/ReactComponents/Header.jsx
+++ src/ReactComponents/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
         
-        <div className="flex md:flex md:justify-center justify-center text-center text-4xl pt-[20px] pl-8 pr-8 md:font-bold bg-gradient-to-r from-blue-600 via-indigo-500 to-violet-400 inline-block text-transparent bg-clip-text sm:font-[1100] font-extrabold select-none my-8 md:my-10 lg:my-10">
+        <div className="flex md:flex md:justify-center justify-center text-center text-4xl pt-[20px] pl-8 pr-8 md:font-bold bg-gradient-to-r from-blue-600 via-indigo-500 to-violet-400 inline-block text-transparent bg-clip-text sm:font-[1100] font-extrabold select-none my-11 md:my-10 lg:my-11">
             <h1 >Your Frequent Taps In One Place.</h1>
         </div>
 
